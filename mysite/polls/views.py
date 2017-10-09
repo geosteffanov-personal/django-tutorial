1ea54e28ece5c87830fd4e5131f35ad67b912ee2
Joe Crown <geosteffanov@gmail.com>
2017-10-09 21:26:16 +0300

    Added mysite project to repository

---
+++ mysite/polls/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello world!")
+
+
+def detail(request, question_id):
+    return HttpResponse("detail %s" % question_id)
+
+def results(request, question_id):
+    return HttpResponse("results %s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("vote %s" % question_id)
+
+
