105dc6c08dbc497733cc5b74415055f8735f0213
Joe Crown <geosteffanov@gmail.com>
2017-10-16 18:29:15 +0300

    Added a more convenient way of assigning choices to questions.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,11 +2,19 @@ from django.contrib import admin
 
 from .models import Question, Choice
 # Register your models here.
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date Information', {'fields' : ['pub_date']})
     ]
+    inlines = [ChoiceInline]
+
+
+
 
 
 
