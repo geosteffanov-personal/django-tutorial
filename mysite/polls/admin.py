0086d276c63ad7d0103f470dd6a3d97719bfe8dd
Joe Crown <geosteffanov@gmail.com>
2017-10-16 18:16:41 +0300

    Split the fields into two fieldsets in the admin form of the Question model.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,11 +3,10 @@ from django.contrib import admin
 from .models import Question, Choice
 # Register your models here.
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
-    # fieldsets = [
-    #     (None, {'fields': ['question_text']}),
-    #     ('Date Information', {'fields' : ['pub_date']})
-    # ]
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields' : ['pub_date']})
+    ]
 
 
 
